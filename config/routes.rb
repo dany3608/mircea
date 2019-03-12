74628df17112accb5171048d2a26053981b5f5d4
Dan Tyekar <dany3608@gmail.com>
2019-03-12 22:48:15 +0200

    api

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
   resources :routes do
   	resources :waypoints
   end
+  namespace :api do
+    namespace :v1 do
+      resources :routes do
+      	resources :waypoints
+    	end
+    end
+end
   get 'homepage/home'
   root 'homepage#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
