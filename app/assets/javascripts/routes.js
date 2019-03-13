528350530d461a10c2b677683b71f38874301c8b
Dan Tyekar <dany3608@gmail.com>
2019-03-13 04:28:29 +0200

    routes & waypoints finished

---
+++ app/assets/javascripts/routes.js
@@ -0,0 +1,34 @@
+      function initMap2(start,end,middle) {
+        var start = new google.maps.LatLng(start[0].x, start[0].y);
+        var end = new google.maps.LatLng(end[0].x, end[0].y)
+
+        var directionsService = new google.maps.DirectionsService;
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 7,
+          center: start
+        });
+        directionsDisplay.setMap(map);
+        
+        var middlePoints = [];
+        var aucs;
+        for (var i = 0; i < middle[0].length; i++) {
+          aucs = new google.maps.LatLng(middle[0][i].x, middle[0][i].y);
+          middlePoints.push({
+              location: aucs,
+              stopover: false
+            });
+        }
+        directionsService.route({
+          origin: start,
+          destination: end,          
+          waypoints: middlePoints,
+          travelMode: 'DRIVING'
+        }, function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      }
