9da760452bb861ecec4b960b79a60bdff36b9d52
Dan Tyekar <dany3608@gmail.com>
2019-03-13 06:18:23 +0200

    final touches

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -16,19 +16,33 @@
           aucs = new google.maps.LatLng(middle[0][i].x, middle[0][i].y);
           middlePoints.push({
               location: aucs,
-              stopover: false
+              stopover: true
             });
         }
         directionsService.route({
           origin: start,
           destination: end,          
+          optimizeWaypoints: true,
           waypoints: middlePoints,
           travelMode: 'DRIVING'
         }, function(response, status) {
           if (status === 'OK') {
             directionsDisplay.setDirections(response);
+            var route = response.routes[0];
+            var summaryPanel = document.getElementById('directions-panel');
+            summaryPanel.innerHTML = '';
+            for (var i = 0; i < route.legs.length; i++) {
+              var routeSegment = i + 1;
+              summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+                  '</b><br>';
+              summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+              summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+              summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+            }
           } else {
             window.alert('Directions request failed due to ' + status);
           }
+          return response; 
         });
+
       }
